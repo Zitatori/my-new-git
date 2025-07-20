bb58235577724f615fc4a7079d68e3ba95639c17
Zitatori <zitatori@gmail.com>
2025-07-20 23:48:14 +0200

    変更内容の説明

--- pong_game/main.py
+++ pong_game/main.py
@@ -10,8 +10,8 @@ screen.bgcolor("black")
 screen.title("Ping Pong Game")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((340, 0))
+l_paddle = Paddle((-340, 0))
 
 
 ball = Ball()
@@ -19,10 +19,10 @@ scoreboard = Scoreboard()
 
 
 screen.listen()
-screen.onkey(r_paddle.go_up,"Up")
-screen.onkey(r_paddle.go_down,"Down")
-screen.onkey(l_paddle.go_up,"w")
-screen.onkey(l_paddle.go_down,"s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
 game_is_on = True
@@ -31,29 +31,23 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    #Detect collision with wall
+    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #Detect collision with r_paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle)< 50 and ball.xcor() <-320:
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-
-    #detect R paddle misses
+    # detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
         scoreboard.l_point()
 
-    #Detect L paddle misses:
+    # Detect L paddle misses:
     if ball.xcor() < -380:
         ball.reset_position()
         scoreboard.r_point()
 
 
-
-
-
-
 screen.exitonclick()
-
